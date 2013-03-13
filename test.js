09ad24f1d9ac0936a0d9f06d666918f0d0333ad1
Ben Savage <btsavage@sibblingz.com>
2013-03-13 03:05:02 -0700

    added drag-drop from desktop for adding reference artwork behind the canvas as a guide layer

--- test.js
+++ test.js
@@ -414,6 +414,42 @@ function onLoaded(){
 	document.getElementById("deleteLayerButton").addEventListener("click", onDeleteLayerButtonClicked);
 	
 	document.getElementById("layers").addEventListener("click", onLayerGridClicked);
+	
+	canvasNode.addEventListener('dragover', handleDragOver, false);
+	canvasNode.addEventListener('drop', handleDrop, false);
+}
+
+function handleDragOver(e){
+	e.stopPropagation();
+	e.preventDefault();
+	e.dataTransfer.dropEffect = 'copy';
+}
+
+function handleDrop(e){
+	e.stopPropagation();
+	e.preventDefault();
+	
+	var files = e.dataTransfer.files;
+	
+	for( var i = 0; i < files.length; i++ ){
+		var file = files[i];
+		doFile( file );
+	}
+}
+
+function doFile(file){
+	console.log("doing: " + file);
+	var reader = new FileReader();
+	
+	reader.onload = function(e){
+		console.log("it loaded!");
+		var imgNode = document.createElement("img");
+		imgNode.src = e.target.result;
+		imgNode.classList.add("relative");
+		document.getElementById("workarea").insertBefore( imgNode, document.getElementById("canvas") );
+	}
+	
+	reader.readAsDataURL(file);
 }
 
 // Assumes that you have already called:
